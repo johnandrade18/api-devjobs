2ab3b690183ef94c8944d16baf95b3cf1fb632d1
John Andrade <johnandrade18@hotmail.com>
2021-10-13 22:37:23 -0500

    first commit

--- index.js
+++ index.js
@@ -27,14 +27,13 @@ app.use(cookieParser());
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-        mongoUrl: process.env.DATABASE || 'mongodb://localhost:27017/devjobs'
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/devjobs'
     })
 }));
 
 app.use('/api', router)
 
 const port = process.env.PORT || 4000;
-app.set("json spaces", 4);
 app.listen(port, () => {
   console.log("Servidor Corriendo en el puerto: " + port);
 });
